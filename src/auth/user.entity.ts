bb2e762e3b631d7d43589c18358592fd21a4e157
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-01-02 14:13:48 +0530

    Production Changes for Heroku

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -5,7 +5,7 @@ import {
   Column,
   OneToMany,
 } from 'typeorm';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { Task } from 'src/tasks/task.entity';
 
 @Entity()
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ async function bootstrap() {
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
-  if (process.env.NODE_ENV === 'development') app.enableCors();
   const port = process.env.PORT || serverConfig.port;
 
   await app.listen(port);
