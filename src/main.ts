6157af670e2944ce0cf58a54203aa0370537c621
Shaswat Saxena <shaswatsaxena@gmail.com>
2020-01-02 15:05:40 +0530

    Added API Security measures.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import * as config from 'config';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
   app.enableCors();
+  app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   const port = process.env.PORT || config.get('server.port');
 
